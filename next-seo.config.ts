f9def94f17adfe2989754a5bbf26c293b0a9744f
Hide <hidetsugu0906@icloud.com>
2025-04-17 16:43:38 +0900

    Google確認用ファイル追加

---
+++ next-seo.config.ts
@@ -0,0 +1,19 @@
+// next-seo.config.ts
+const SEO = {
+    title: '有限会社エヌ・ケー | 墨出し・測量',
+    description: '鳥取県の建設測量会社「有限会社エヌ・ケー」では、墨出し業務を中心に土木測量にも対応。信頼と技術で現場を支えます。',
+    openGraph: {
+      type: 'website',
+      locale: 'ja_JP',
+      url: 'https://nk-hp.com', // ドメイン変更予定なら後で更新
+      site_name: '有限会社エヌ・ケー',
+    },
+    twitter: {
+      handle: '@yourhandle',
+      site: '@yourhandle',
+      cardType: 'summary_large_image',
+    },
+  };
+  
+  export default SEO;
+  
