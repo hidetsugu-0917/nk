a6b9f2c6851c9c5d2c7cf7c478144f381dc5f8d3
Hide <hidetsugu0906@icloud.com>
2025-04-17 16:51:42 +0900

    thanksページを追加

---
+++ pages/thanks.tsx
@@ -0,0 +1,10 @@
+// pages/thanks.tsx
+export default function ThanksPage() {
+    return (
+      <main style={{ padding: "2rem" }}>
+        <h1>送信ありがとうございました</h1>
+        <p>お問い合わせ内容を受け付けました。</p>
+      </main>
+    );
+  }
+  
